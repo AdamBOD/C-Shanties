5272693c69f8da690370e6cbe9a2735ce7495d66
AdamBOD <adambarryodonovan@gmail.com>
2019-10-20 16:10:34 +0100

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { IonRangeSliderModule } from 'ng2-ion-range-slider';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ControlCentreComponent } from './control-centre/control-centre.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ControlCentreComponent } from './control-centre/control-centre.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    IonRangeSliderModule
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
