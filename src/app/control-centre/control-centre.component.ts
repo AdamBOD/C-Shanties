d85513c01a117370d5375412d49e82882f6e9316
AdamBOD <adambarryodonovan@gmail.com>
2019-10-20 23:19:31 +0100

    Updated control centre

--- src/app/control-centre/control-centre.component.ts
+++ src/app/control-centre/control-centre.component.ts
@@ -7,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
 export class ControlCentreComponent implements OnInit {
   private startTime = "0:00";
   private endTime = "3:25";
+  private playing = false;
+  private currentTime = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public togglePlay(event: Event): void {
+    this.playing = !this.playing;
+
+    window.setInterval(() => {
+      if (this.playing) {
+        console.log(this.currentTime);
+        this.currentTime++;
+      }
+    }, 1000)
+  }
 }
