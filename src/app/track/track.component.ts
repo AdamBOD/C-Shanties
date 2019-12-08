2e74930468ad2fb7034e45b4669b644f2dc05ee5
AdamBOD <adambarryodonovan@gmail.com>
2019-12-08 19:01:05 +0000

    Added track list

--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-track',
-  templateUrl: './track.component.html'
+    selector: '[app-track]',
+    templateUrl: './track.component.html'
 })
 export class TrackComponent implements OnInit {
+    @Input() trackData;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
